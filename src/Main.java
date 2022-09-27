57c30fc60a4b388b53a944decdadcd49d3dcf8f5
Sozonchik <den.sozon@mail.ru>
2022-09-27 17:09:41 +0500

    Сделал задание 3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        int humanAge = 33;
+        if (humanAge >= 2 && humanAge <= 6) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в детский сад");
+        } else if (humanAge >= 7 && humanAge <= 18) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в школу");
+        } else if (humanAge > 18 && humanAge < 24) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то его место в университете");
+        } else {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему пора ходить на работу");
+
+        }
+
+        System.out.println("Задание 2");
+
     }
 }
