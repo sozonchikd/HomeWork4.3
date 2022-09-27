f0b5244236a553a35aaba5ed03f0886ff344e7a3
Sozonchik <den.sozon@mail.ru>
2022-09-27 17:37:09 +0500

    Сделал задание 3.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int humanAge = 33;
+        int humanAge = 19;
         if (humanAge >= 2 && humanAge <= 6) {
             System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в детский сад");
         } else if (humanAge >= 7 && humanAge <= 18) {
@@ -14,6 +14,13 @@ public class Main {
         }
 
         System.out.println("Задание 2");
-
+        int childAge = 14;
+        if (childAge < 5) {
+            System.out.println("Ребёнку нельзя кататься на атракционе если ему " + childAge + " года.");
+        } else if (childAge >= 5 && childAge < 14) {
+            System.out.println("Если ребёнку " + childAge + " лет то ему можно кататься на атракционе только в сопровождении взрослого.");
+        } else {
+            System.out.println("Если ребёнку " + childAge + " лет, то он может кататься без сопровождения взрослого.");
+        }
     }
 }
